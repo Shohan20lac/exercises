538804b11528082f50e1d5aae71868deca13429d
Shohan Rahman <shohan.rahman@chaldal.net>
2023-02-06 15:36:47 +0600

    Update prob2.fsx
    done

--- prob2.fsx
+++ prob2.fsx
@@ -1,35 +1,28 @@
 //Project Euler Problem #2: 
 // find the sum of the even-valued fibonacci numbers (Starting with 1 and 2) 
-
-let rec simplePatternMatch (x:int): bool =
+let rec simplePatternMatch x =
    match x with
-    | 10 -> true
-    | _ -> false || simplePatternMatch (x-10)
+    | 10 -> printfn "inputval is 10."
+    | _ -> 
+        printfn "inputval is not 10. Decreasing..."
+        simplePatternMatch (x-10)
 
-let rec factorial (x:int) : int =
+let isEven x=
+    if x%2=0 then x
+    else 0
+
+let rec factorial x =
     match x with
     | 1 -> 1
     | _ ->
         x * factorial (x-1)
 
-let oddNumbersIntoZero (x:int) : int =
-    if x%2=0 then x
-    else 0
-
-let rec fib (n:int) : int =
+let rec fib n=
     match n with
-    | 1 -> 1
-    | 2 -> 2
-    | _ -> fib(n-1) + fib(n-2)
+    |2 -> 2
+    |1 -> 1
+    |_ -> fib(n-1) + fib(n-2)
 
 let stopWatch = System.Diagnostics.Stopwatch.StartNew()
-[1..31] 
-|> Seq.map fib 
-|> Seq.map oddNumbersIntoZero 
-|> Seq.sum 
-|> printfn "%A"
+[1..30] |> Seq.map fib |> Seq.map isEven |> Seq.sum |> printfn "%A"
 stopWatch.Stop()
-printfn "%f ms" stopWatch.Elapsed.TotalMilliseconds
-
-
-
