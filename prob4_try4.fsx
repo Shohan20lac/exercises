c5b34fb7881739aa143b95073589b51fc3f34328
Shohan Rahman <shohan.rahman@chaldal.net>
2023-02-15 17:24:26 +0600

    prob 4 WRAP UP
    cleaned up and ready to be pushed

--- prob4_try4.fsx
+++ prob4_try4.fsx
@@ -1,20 +1,10 @@
-//HEAD-ON APPROACH B: GENERATE FILTERED
+// Euler Problem 4:
 
-// step1: define a function that does this:
+// A palindromic number reads the same both ways.
+// The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
-// given length,
-// for i in range[1..length]
-    // generate [ (i,i) (i,i+1) ... (i,length) ]
+// Find the largest palindrome made from the product of two 3-digit numbers.
 
-let rec factorsStartingFrom (x: uint) (y: uint) (n: uint) :seq<uint * uint>=
-    seq {
-        if x < y then
-            if n % x = (uint 0) then
-                yield x , (n/x)
-
-            if x < n/(uint 2) then
-                yield! factorsStartingFrom (x+(uint 1)) y n 
-    }
 let rec smallestNumberBackwards (digitCount: int) =
     if digitCount > 1 then
         (string (0) + smallestNumberBackwards(digitCount-1))
@@ -31,6 +21,7 @@ let reverseString (v: string) =
     |> Seq.rev
     |> Seq.map string
     |> String.concat ""
+    
 let isPalindrome (v:int): bool = 
     let reversed = 
         v
@@ -44,21 +35,17 @@ let minFactor =
     smallestNumberBackwards 3
     |> reverseString
     |> int
-printfn $"{minFactor}"
+    
 let maxFactor =
     largestNumber 3
     |> int
-printfn $"{maxFactor}"
-let length = Seq.length [minFactor..maxFactor]
-printfn $"length: {length}"
-
+    
 let stopWatch = System.Diagnostics.Stopwatch.StartNew()
 let result =
     [minFactor..maxFactor]
     |> Seq.map(
         fun i ->
             let iReplicated = Seq.replicate ((maxFactor-1)) (i)
-            // printfn "replicated: %A" iReplicated
             Seq.zip iReplicated (Seq.skip (i-1)  [1..(maxFactor)])
         )
     |> Seq.collect ( fun seq-> seq )
@@ -66,11 +53,13 @@ let result =
         fun (number1,number2) ->
             number1 * number2
             )
-    // |> Seq.contains 906609
     |> Seq.filter isPalindrome
     |> Seq.max
-
 stopWatch.Stop()
 
-printfn $"{result}"
+printfn $"Answer: {result}"
 printfn $"Run time: {stopWatch.Elapsed.TotalMilliseconds} ms"
+
+// // Output:
+// Answer: 906609
+// Run time: 253.9017 ms
