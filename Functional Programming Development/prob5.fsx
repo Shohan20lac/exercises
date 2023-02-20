6d2d1b99d4349a1ddb5fccf17d3b0355309a8d38
Shohan Rahman <shohan.rahman@chaldal.net>
2023-02-20 14:40:08 +0600

    Create prob5.fsx
    Started prob5

---
+++ Functional Programming Development/prob5.fsx
@@ -0,0 +1,14 @@
+﻿// Problem 5
+// 2520 is the smallest number that can be
+// divided by each of the numbers from 1 to 10
+// without any remainder.
+//
+// What is the smallest positive number that is
+// evenly divisible by all of the numbers from 1 to 20?
+
+// Fully brute force:
+
+let myset = Set [1..20]
+
+printfn $"{myset}"
+
